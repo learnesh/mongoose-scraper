e1bd124f1476d3307543159828274499f01cbd36
learnesh <32178001+learnesh@users.noreply.github.com>
2018-04-14 00:23:29 -0500

    Create article.js

---
+++ article.js
@@ -0,0 +1,28 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
